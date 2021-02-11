26314751715278d153064db0b8d179f2839a4c0a
colin <colin.loury@gmail.com>
2021-02-11 16:08:10 +0100

    fin du premier test

--- cours-2/src/app/heroes/heroes.component.ts
+++ cours-2/src/app/heroes/heroes.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -6,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  hero = 'Windstorm';
+  hero: Hero = {
+    id: 1,
+    name: 'Windstrom'
+  };
+  heroes = HEROES;
 
   constructor() { }
 
